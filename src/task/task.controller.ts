c5778d0a05e3fae87dd35ee3900699e3a29647e5
Fernando Rodrigues Junior <fernandojr0227@gmail.com>
2024-07-24 21:58:07 -0300

    commit

---
+++ src/task/task.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { TaskDto } from './task.dto';
+import { TaskService } from './task.service';
+
+@Controller('task')
+export class TaskController {
+    constructor(private readonly taskService: TaskService){}
+
+    @Post()
+    create(@Body() task: TaskDto){
+        this.taskService.create(task);
+    }
+
+    @Get('/:id')
+    findById(@Param('id') id: string){
+        this.taskService.findById(id);    
+    }
+
+    @Put()
+    update(@Body() task: TaskDto){
+        this.taskService.update(task);
+    }
+}
